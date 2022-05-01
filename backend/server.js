601162fe050ed01c25937de210fa35a9581e7ae3
Emmanuel Omonzebaguan <codecps@Emmanuels-MacBook-Pro.local>
2022-05-01 22:45:48 +0100

    install heroku cli and deploy to heroku, set the app to production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -13,13 +14,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message:'hello, welcome to support system api!'})
-})
-
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+// Serve Frontend
+if(process.env.NODE_ENV === 'production'){
+    // set build folder as statuc
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(__dirname, '../frontend', 'build', 'index.html'))
+}else{
+    app.get('/', (req, res) => {
+        res.status(200).json({message:'hello, welcome to support system api!'})
+    })
+}
 
 app.use(errorHandler)
 
